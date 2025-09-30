dc00d689a8810357a4bee6a0b1b69d531195a147
dageta <davi.linhares.nascimento@escola.pr.gov.br>
2025-09-30 10:51:35 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Exibe um alerta no console do navegador quando a página carrega
+console.log("O site carregou e o JavaScript está funcionando!");
 
+// Adiciona uma funcionalidade a um elemento
+// Se houver um botão com o ID "meuBotao", ele fará algo ao ser clicado
+/*
+document.getElementById('meuBotao').addEventListener('click', function() {
+    alert('Você clicou no botão!');
+});
+*/
