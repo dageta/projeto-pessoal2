c00dcbdbd3de18d723d1135a4969dbaf0293889b
dageta <davi.linhares.nascimento@escola.pr.gov.br>
2025-10-01 18:11:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ console.log("O site carregou e o JavaScript está funcionando!");
 document.getElementById('meuBotao').addEventListener('click', function() {
     alert('Você clicou no botão!');
 });
-*/
+
